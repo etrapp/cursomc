a7beb08b64800e9056eb60cd8b0c3e19c9458071
etrapp <etrapp@gmail.com>
2020-07-21 07:04:58 -0300

    Criando instanciação de objetos iniciais de Categoria

--- src/main/java/com/trapp/cursomc/CursomcApplication.java
+++ src/main/java/com/trapp/cursomc/CursomcApplication.java
@@ -1,14 +1,31 @@
 package com.trapp.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class CursomcApplication extends SpringBootServletInitializer {
+import com.trapp.cursomc.domain.Categoria;
+import com.trapp.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication extends SpringBootServletInitializer implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática"); 
+		Categoria cat2 = new Categoria(null, "Escritório"); 
+		repo.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
