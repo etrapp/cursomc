337e2593473bbd8cfe60a2bbc429747fef0fb2f9
etrapp <etrapp@gmail.com>
2020-07-23 06:27:17 -0300

    Tratamento de exceção

--- src/main/java/com/trapp/cursomc/CursomcApplication.java
+++ src/main/java/com/trapp/cursomc/CursomcApplication.java
@@ -9,13 +9,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.trapp.cursomc.domain.Categoria;
+import com.trapp.cursomc.domain.Produto;
 import com.trapp.cursomc.repositories.CategoriaRepository;
+import com.trapp.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 	@Autowired
-	private CategoriaRepository repo;
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -25,7 +30,21 @@ public class CursomcApplication extends SpringBootServletInitializer implements 
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática"); 
 		Categoria cat2 = new Categoria(null, "Escritório"); 
-		repo.saveAll(Arrays.asList(cat1,cat2));
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 	}
 
 }
--- src/main/java/com/trapp/cursomc/services/CategoriaService.java
+++ src/main/java/com/trapp/cursomc/services/CategoriaService.java
@@ -2,10 +2,12 @@ package com.trapp.cursomc.services;
 
 import java.util.Optional;
 
+//import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.trapp.cursomc.domain.Categoria;
+import com.trapp.cursomc.exceptions.ObjectNotFoundException;
 import com.trapp.cursomc.repositories.CategoriaRepository;
 
 @Service
@@ -16,7 +18,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> categoria = repo.findById(id);
-		return categoria.orElse(null);
+		//return categoria.orElse(null);
+		return categoria.orElseThrow(()->new ObjectNotFoundException("Objeto não encontrado! Id:" + id + " Tipo:" + Categoria.class.getName()));
 	}
 	
 
