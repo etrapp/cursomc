2703dd50b10777ffbf7c28bea1af87a1203610c6
etrapp <etrapp@gmail.com>
2020-07-23 07:03:10 -0300

    Cidade e estado

--- src/main/java/com/trapp/cursomc/CursomcApplication.java
+++ src/main/java/com/trapp/cursomc/CursomcApplication.java
@@ -9,8 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.trapp.cursomc.domain.Categoria;
+import com.trapp.cursomc.domain.Cidade;
+import com.trapp.cursomc.domain.Estado;
 import com.trapp.cursomc.domain.Produto;
 import com.trapp.cursomc.repositories.CategoriaRepository;
+import com.trapp.cursomc.repositories.CidadeRepository;
+import com.trapp.cursomc.repositories.EstadoRepository;
 import com.trapp.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,7 +25,13 @@ public class CursomcApplication extends SpringBootServletInitializer implements 
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -42,8 +52,20 @@ public class CursomcApplication extends SpringBootServletInitializer implements 
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
 	}
 
---
+++ src/main/java/com/trapp/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.trapp.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.trapp.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
